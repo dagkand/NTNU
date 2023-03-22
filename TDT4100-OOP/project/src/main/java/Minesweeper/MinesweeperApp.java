dbd08d19075f91bcf940b5d6a0acea309181a166
dagkand <dagkand@ntnu.no>
2023-03-22 17:51:19 +0100

    added examplefiles

---
+++ TDT4100-OOP/project/src/main/java/Minesweeper/MinesweeperApp.java
@@ -0,0 +1,23 @@
+package Minesweeper;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MinesweeperApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Example App");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("App.fxml"))));
+        primaryStage.show();
+    }
+
+}
